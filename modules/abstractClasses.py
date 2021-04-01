a0fb3e54681ed65641ff38a09bbd9bdd99e9bcb2
Leo Zeitler <leozeitler95@gmail.com>
2021-04-01 19:20:11 +0200

    Update: Abstract Protein, Complex and DNA class
    Makes it possible to add security checks and avoid cyclic
    imports

---
+++ modules/abstractClasses.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from abc import ABC
+
+
+class AbstractProtein(ABC):
+    pass
+
+
+class AbstractProteinComplex(ABC):
+    pass
+
+
+class AbstractDNASegment(ABC):
+    pass
+
