e9cc1c66227f50e237cf5c05f6c5561bf577b7f5
Leo Zeitler <leozeitler95@gmail.com>
2021-04-06 20:23:18 +0200

    Update: Segments and DNA molecule
    Implemented class to define DNA segments (e.g. lesion or core
    promoter) and the DNA molecule that holds all segments

--- modules/dna.py
+++ modules/dna.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
 import numpy as np
 from modules.abstractClasses import *
+from modules.messengers import Message
 
 
 class DNASegment(AbstractDNASegment):
     SEGMENT_UNIT = .01
 
-    def __init__(self, start, stop):
+    def __init__(self, start, stop, message, sc=None, tc=None):
         self.start = start
         self.stop = stop
         self.species = '%s:%s' % (self.start, self.stop)
+        self.message = message
+        if sc is not None:
+            if not isinstance(sc, AbstractEvent):
+                raise ValueError('Start condition is not None and not an event.')
+        if tc is not None:
+            if not isinstance(tc, AbstractEvent):
+                raise ValueError('Termination condition is not None and not an event.')
+        self.sc = sc
+        self.tc = tc
         self.proteins = []
-        self.messages = {}
-        self.cond = {}
 
     def get_position(self):
         x = np.arange(self.start, self.stop, DNASegment.SEGMENT_UNIT)
         y = np.repeat(.5, x.size)
-        return np.dstack((x, y))
+        return np.dstack((x, y))[0]
 
     def add_protein(self, p):
         self.proteins.append(p)
@@ -25,6 +33,36 @@ class DNASegment(AbstractDNASegment):
     def del_protein(self, p):
         self.proteins.remove(p)
 
-    def add_event(self, target, number):
-        pass
+    def dissociate(self):
+        dissoc_prot = []
+        for p in self.proteins:
+            if not p.interact(self):
+                dissoc_prot.append(p)
+                self.del_protein(p)
+
+        return dissoc_prot
+
+    def emit(self):
+        if self.sc is not None:
+            if self.sc(self.proteins):
+                self.sc = None
+                return self.message
+        else:
+            if self.tc is None:
+                return self.message
+            else:
+                if not self.tc(self.proteins):
+                    return self.message
+
+
+class DNA:
+    def __init__(self):
+        self.dna_segments = []
+
+    def __iter__(self):
+        return self.dna_segments.__iter__()
+
+    def add_segment(self, start, stop, target, new_prob, sc=None, tc=None):
+        message = Message(target, '%s:%s' % (start, stop), new_prob)
+        self.dna_segments.append(DNASegment(start, stop, message, sc=sc, tc=tc))
 
